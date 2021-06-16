7cf28fd28b4ffe7f683ca5826a99fd6a47f9d682
默存 冯 <793962618@qq.com>
2021-06-16 23:06:37 +0800

    测试

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Pharmacy_management
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
